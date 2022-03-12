b691874f53e07ba82c7e633839d2443217d3108e
ElenoraZhen <00tweety00wu@gmail.com>
2022-03-12 20:27:03 +0800

    init project

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,101 @@
+module.exports = { 
+    title: "Elenora_.z", 
+    description: "Build from quickStart4Vuepress, made from CXPhoenix", 
+    themeConfig: {
+        logo: 'https://imglf6.lf127.net/img/SGhwMFNNWmJHaGJhWVkrcld5OW9lM1BhZUU4cEp5Qlk5L3FMd0JveXo2Z1RIVnpIQ2UvdFl3PT0.gif?imageView&thumbnail=1000x0&tostatic=0',
+      },
+      themeConfig: {
+        // 可折叠的侧边栏
+        sidebar: {
+          '/note/': [
+            {
+              text: '建網頁前',
+              collapsible: true,
+              children: ['/note/readme.md', '/note/Readme0312.md'],
+            },
+            {
+              text: '建立網頁',
+              collapsible: true,
+              children: ['/note2/readme.md', '/note2/readme2.md'],
+            },
+            {
+              text: 'Github 及 Vercel 上架',
+              collapsible: true,
+              children: ['/note3/readme.md', '/note3/readme2.md'],
+            },
+            {
+              text: '最後的最後!',
+              collapsible: true,
+              children: ['/note4/readme.md'],
+            },
+          ],
+          '/note2/': [
+            {
+              text: '建網頁前',
+              collapsible: true,
+              children: ['/note/readme.md', '/note/Readme0312.md'],
+            },
+            {
+              text: '建立網頁',
+              collapsible: true,
+              children: ['/note2/readme.md', '/note2/readme2.md'],
+            },
+            {
+              text: 'Github 及 Vercel 上架',
+              collapsible: true,
+              children: ['/note3/readme.md', '/note3/readme2.md'],
+            },
+            {
+              text: '最後的最後!',
+              collapsible: true,
+              children: ['/note4/readme.md'],
+            },
+          ],
+          '/note3/': [
+            {
+              text: '建網頁前',
+              collapsible: true,
+              children: ['/note/readme.md', '/note/Readme0312.md'],
+            },
+            {
+              text: '建立網頁',
+              collapsible: true,
+              children: ['/note2/readme.md', '/note2/readme2.md'],
+            },
+            {
+              text: 'Github 及 Vercel 上架',
+              collapsible: true,
+              children: ['/note3/readme.md', '/note3/readme2.md'],
+            },
+            {
+              text: '最後的最後!',
+              collapsible: true,
+              children: ['/note4/readme.md'],
+            },
+          ],
+          '/note4/': [
+            {
+              text: '建網頁前',
+              collapsible: true,
+              children: ['/note/readme.md', '/note/Readme0312.md'],
+            },
+            {
+              text: '建立網頁',
+              collapsible: true,
+              children: ['/note2/readme.md', '/note2/readme2.md'],
+            },
+            {
+              text: 'Github 及 Vercel 上架',
+              collapsible: true,
+              children: ['/note3/readme.md', '/note3/readme2.md'],
+            },
+            {
+              text: '最後的最後!',
+              collapsible: true,
+              children: ['/note4/readme.md'],
+            },
+          ]
+        },
+      },
+
+}; 
---
+++ docs/README.md
@@ -0,0 +1,18 @@
+---
+home: true
+heroText: Elenora's note
+tagline: 應該做的還可以(??!可以切換黑夜模式看看ww
+heroImage: https://imglf6.lf127.net/img/VmJZekNtak9SRkFHM1prRldraDFrcnV0YVVXRDhhakdYcnJnVTlURmlWTDlTaVBBV3g2SGtRPT0.jpg?imageView&thumbnail=1000x0&type=jpg
+heroImageDark : https://imglf6.lf127.net/img/MENTbEZNK2xWUlRFT0N5cFkxRlg4TXdmQjFENVpMTnNNaHJ2azZxM1d4UzNVSWJ0VC9NWTV3PT0.jpg?imageView&thumbnail=500x0&quality=96&stripmeta=0&type=jpg
+actions:
+  - text: Go
+    link: /note/
+    type: primary
+features:
+  - title: 介紹一下
+    details: 噢噢，我就是一個原耽女孩，所有放的圖片都是原耽圈的!!(雖然只有兩張ww
+  - title: 我想說
+    details: 這個真的做了有夠久的ww，為甚麼我可以這麼電腦白癡ㅍ_ㅍ
+  - title: 不專業發言
+    details: 這堂課真的很有用ㄟ，我打算架一個接稿平台，用這個看起來比用 lit.link 專業多了(有吧?!!!
+---
