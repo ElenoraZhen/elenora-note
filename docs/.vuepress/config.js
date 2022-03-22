7db60a493e5c9a4111620e581600204a48f6b023
ElenoraZhen <00tweety00wu@gmail.com>
2022-03-22 22:18:57 +0800

    新增html & css & js & 實作 profile card 筆記

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -24,6 +24,11 @@ module.exports = {
               children: ['/note/note3/', '/note/note3/readme2.md'],
             },
             {
+              text: 'html & css & js',
+              collapsible: true,
+              children: ['/note/note3-1/','/note/note3-1/readme2.md', '/note/note3-1/readme3.md','/note/note3-1/readme4.md'],
+            },
+            {
               text: '最後的最後!',
               collapsible: true,
               children: ['/note/note4/'],
